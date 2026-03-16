e6ddfbe5b215a6bceb2cc87d05f04cce5262adf3
Yash Padad <yash.padad@pif.org.in>
2026-03-16 19:21:35 +0530

    changes updated

--- src/app/layout/Happiness Teacher/happinessregistrationform/happinessregistrationform.component.ts
+++ src/app/layout/Happiness Teacher/happinessregistrationform/happinessregistrationform.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CalpifService } from 'src/app/shared/services/calpif.service';
@@ -8,7 +8,8 @@ import { CalpifService } from 'src/app/shared/services/calpif.service';
   templateUrl: './happinessregistrationform.component.html',
   styleUrls: ['./happinessregistrationform.component.scss']
 })
-export class HappinessregistrationformComponent {
+export class HappinessregistrationformComponent implements OnInit {
+
   teacherForm: FormGroup;
   isSubmitting = false;
 
@@ -17,10 +18,18 @@ export class HappinessregistrationformComponent {
   district;
   school;
 
-  constructor(private fb: FormBuilder, private service: CalpifService, private router: Router) { }
+  standardOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  constructor(
+    private fb: FormBuilder,
+    private service: CalpifService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+
     this.teacherForm = this.fb.group({
+
       teachername: ['', Validators.required],
       mobileno: ['', [Validators.required, Validators.pattern('^[6-9][0-9]{9}$')]],
       email: ['', [Validators.required, Validators.email]],
@@ -28,82 +37,139 @@ export class HappinessregistrationformComponent {
 
       schoolname: ['', Validators.required],
       udiseno: ['', Validators.required],
-      standard: ['', Validators.required],
+
+      standard: [[], Validators.required],
+
       state: ['', Validators.required],
       district: ['', Validators.required],
       city: ['', Validators.required],
 
-      teacherdesignation: ['', Validators.required],
+      teacherdesignation: ['', Validators.required]
+
     });
 
-    this.service.FillDropDown('HappinessSchoolwiseData', 'distinct state', 'state', '').subscribe((res: any) => {
-      this.state = res;
-    });
-    this.service.FillDropDown('HappinessSchoolwiseData', 'distinct district', 'district', '').subscribe((res: any) => {
-      this.district = res;
-    });
-    this.service.FillDropDown('HappinessSchoolwiseData', 'distinct city', 'city', '').subscribe((res: any) => {
-      this.city = res;
-    });
-    this.service.FillDropDown('HappinessSchoolwiseData', 'distinct schoolname', 'schoolid ', '').subscribe((res: any) => {
-      this.school = res;
-    });
+    this.loadStates();
+
+  }
+
+  loadStates() {
+
+    this.service.FillDropDown('HappinessSchoolwiseData', 'distinct state', 'state', '')
+      .subscribe((res: any) => {
+        this.state = res;
+      });
+
   }
 
   onStateChange(stateValue) {
-    this.service.FillDropDown('HappinessSchoolwiseData', 'distinct district', 'district', "where state='" + stateValue + "'").subscribe((res: any) => {
-      this.district = res;
-    });
-    this.service.FillDropDown('HappinessSchoolwiseData', 'distinct city', 'city', "where state='" + stateValue + "'").subscribe((res: any) => {
-      this.city = res;
-    });
-    this.service.FillDropDown('HappinessSchoolwiseData', 'distinct schoolname', 'schoolid ', "where state='" + stateValue + "'").subscribe((res: any) => {
-      this.school = res;
-    });
+
+    this.service.FillDropDown('HappinessSchoolwiseData', 'distinct district', 'district', "where state='" + stateValue + "'")
+      .subscribe((res: any) => { this.district = res });
+
+    this.service.FillDropDown('HappinessSchoolwiseData', 'distinct city', 'city', "where state='" + stateValue + "'")
+      .subscribe((res: any) => { this.city = res });
+
+    this.service.FillDropDown('HappinessSchoolwiseData', 'distinct schoolname', 'schoolid', "where state='" + stateValue + "'")
+      .subscribe((res: any) => { this.school = res });
+
   }
 
   onDistrictChange(districtValue) {
-    this.service.FillDropDown('HappinessSchoolwiseData', 'distinct city', 'city', "where district='" + districtValue + "'").subscribe((res: any) => {
-      this.city = res;
-    });
-    this.service.FillDropDown('HappinessSchoolwiseData', 'distinct schoolname', 'schoolid ', "where district='" + districtValue + "'").subscribe((res: any) => {
-      this.school = res;
-    });
+
+    this.service.FillDropDown('HappinessSchoolwiseData', 'distinct city', 'city', "where district='" + districtValue + "'")
+      .subscribe((res: any) => { this.city = res });
+
+    this.service.FillDropDown('HappinessSchoolwiseData', 'distinct schoolname', 'schoolid', "where district='" + districtValue + "'")
+      .subscribe((res: any) => { this.school = res });
+
   }
 
   onCityChange(cityValue) {
-    this.service.FillDropDown('HappinessSchoolwiseData', 'distinct schoolname', 'schoolid ', "where city='" + cityValue + "'").subscribe((res: any) => {
-      this.school = res;
-    });
+
+    this.service.FillDropDown('HappinessSchoolwiseData', 'distinct schoolname', 'schoolid', "where city='" + cityValue + "'")
+      .subscribe((res: any) => { this.school = res });
+
   }
 
   onSchoolChange(schoolValue) {
-    this.service.FillDropDown('HappinessSchoolwiseData', 'distinct udiseno', 'udiseno', "where schoolid='" + schoolValue + "'").subscribe((res: any) => {      
-      this.teacherForm.patchValue({ udiseno: res[0].column1 });
+
+    this.service.FillDropDown('HappinessSchoolwiseData', 'distinct udiseno', 'udiseno', "where schoolname='" + schoolValue + "'")
+      .subscribe((res: any) => {
+
+        this.teacherForm.patchValue({
+          udiseno: res[0].column1
+        });
+
+      });
+
+  }
+
+  isStandardSelected(option: number): boolean {
+
+    const selected = this.teacherForm.get('standard')?.value || [];
+    return selected.includes(option);
+
+  }
+
+  toggleStandard(option: number, checked: boolean) {
+
+    const standards = this.teacherForm.get('standard')?.value || [];
+
+    if (checked) {
+      standards.push(option);
+    }
+    else {
+      const index = standards.indexOf(option);
+      if (index > -1) {
+        standards.splice(index, 1);
+      }
+    }
+
+    this.teacherForm.patchValue({
+      standard: standards
     });
+
+    this.teacherForm.get('standard')?.markAsTouched();
+
   }
 
   submitForm() {
 
     if (this.teacherForm.valid) {
+
       this.isSubmitting = true;
 
-      this.service.TeacherRegistrationHappiness(this.teacherForm.value).subscribe(
-        (res) => {
+      const formData = {
+        ...this.teacherForm.value,
+        standard: this.teacherForm.value.standard.join(',')
+      };
+
+      this.service.TeacherRegistrationHappiness(formData)
+        .subscribe((res) => {
+
+          this.isSubmitting = false;
+
           if (res == 'Teacher registered successfully') {
-            this.isSubmitting = false;
+
             alert('Teacher Registered Successfully');
+
             this.teacherForm.reset();
-            this.router.navigate(["/login"]);
+
+            this.router.navigate(['/login']);
+
           }
           else {
             alert('Registration failed');
           }
+
         });
+
     }
+
   }
 
   get f() {
     return this.teacherForm.controls;
   }
+
 }
